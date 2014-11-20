a81e8548684983210a00f146250268ee659096e4
alexisrendon <alexis9229@hotmail.com>
2014-11-20 14:52:43 -0500

    html fixed

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
 $(function(){
 	var windowHeight=$(window).height();
-		$('.side, .page').css({
-		  height:windowHeight+'px'
-		});
-
-	$(window).resize(function(){
-	  var windowHeight=$(window).height();
-	    $('.side, .page').css({
-	        height:windowHeight+'px'
-		});
+	$('.side, .page').css({
+	  height:windowHeight+'px'
 	});
 
+	$(window).resize(function(){
+		var windowHeight=$(window).height();
+		$('.side, .page').css({
+			height:windowHeight+'px'
+		});
+	});
 });
